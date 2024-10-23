fb6c743ee06abfe220b2b4a086ba41db8edd65b6
Adarsh Yadav <yadavadarsh061@gmail.com>
2024-10-23 07:57:08 +0530

    major commit

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+mongoose.set('debug', true); // Enable detailed MongoDB queries logging
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
+
